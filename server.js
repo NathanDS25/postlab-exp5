60d751596903ae6adcf5e13ea74914ccf9799657
Nathan <dsouzanathan25@gmail.com>
2026-04-10 00:42:27 +0530

    Fix Node static resolution

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
@@ -10,8 +11,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public')); // Serve frontend UI
+app.use(express.static(path.join(__dirname, 'public'))); // Serve frontend UI correctly in Vercel Serverless
 
+// Fallback to ensure Vercel loads the index page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Routes
 app.use('/api/users', userRoutes);
